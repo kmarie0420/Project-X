5e6220fe76de27e82763096b1e1186fe87680d61
Mark Anthony <manthonyamaro@yahoo.com>
2023-08-01 20:55:59 -0500

    added styling, handlebars set ups and set to push to heroku for database adjustments using jawsdatabase will adding login route to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,27 +2,33 @@ const router = require('express').Router();
 const { Sleep, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// findAll
 router.get('/', async (req, res) => {
   try {
-  const sleepData = await Sleep.findAll({
-    include: [
-      {
-      model: sleep, 
-      attributes: ['id', 'sleepDuration', 'wakeUpCount', 'heartRate', 'heartRateVariability', 'respiration', 'snoring', 'timeSleeping', 'sleepInterruptions', 'bodyTemperature'],
-      }, 
-  ],
-});
+    const sleepData = await Sleep.findAll({
+      include: [
+        {
+          model: sleep, 
+          attributes: ['id', 'sleepDuration', 'wakeUpCount', 'heartRate', 'heartRateVariability', 'respiration', 'snoring', 'timeSleeping', 'sleepInterruptions', 'bodyTemperature'],
+        }, 
+      ],
+    });
 
-const sleep = sleepData.map((sleep) => sleep.get({ plain: true }));
-res.render('homepage', {
-  sleep
-});
+    const sleep = sleepData.map((sleep) => sleep.get({ plain: true }));
+    res.render('homepage', {
+      sleep,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
-  });
+});
 
-
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 
 module.exports = router;
